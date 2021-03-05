81cc1610244ab3aa0d8ef0df0a7eb9a659f33cea
James Galantino <james_galantino@aol.com>
2021-03-05 17:11:22 -0500

    Added post request route for workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,20 @@
 const Router = require("express").Router();
 const db = require("../models");
 
+// CREATE
+// create a workout
+Router.post("/api/workouts", async ({ body }, res) => {
+    try {
+        const newWorkout = await db.Workout.create(body);
+        res.send(newWorkout);
+        console.log(newWorkout);
+    } catch (err) {
+        throw new Error(err);
+    };
+});
+
+// READ
+// get last workout
 Router.get("/api/workouts", async (req, res) => {
     try {
       const workouts = await db.Workout.aggregate([
@@ -13,23 +27,30 @@ Router.get("/api/workouts", async (req, res) => {
      res.send(workouts);
     } catch (err) {
       throw new Error(err);
-    }
+    };
 });
 
+// get stats from workout
 Router.get("/api/workouts/range", async (req, res) => {
     try {
       const stats = await db.Workout.aggregate([
         { 
-        $addFields: {
-            totalDuration: { $sum: "$exercises.duration" },
-            totalWeight: { $sum: "$exercises.weight" }
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+                totalWeight: { $sum: "$exercises.weight" }
+            },
         },
-      },
+        { $limit: 7 }
      ])
      res.send(stats);
     } catch (err) {
       throw new Error(err);
-    }
+    };
 });
+
+// UPDATE
+
+// DELETE
+
 
 module.exports = Router;
