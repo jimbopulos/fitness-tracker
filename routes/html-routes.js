a1ccfe002e48af45f1d08cd5b90a66786bd07d17
James Galantino <james_galantino@aol.com>
2021-03-05 16:00:20 -0500

    Added stats html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,18 +1,13 @@
 const path = require("path");
 const Router = require("express").Router();
-// require("../public/exercise")
 
 // Routes to display html
 Router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  });
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
-// Router.get("/exercise?", (req, res) => {
-//   res.send(exercise.html);
-// });  
-
-// Router.get("/exercise", (req, res) => {
-//     res.send(path.join(__dirname, "public"));
-// });
+Router.get("/stats", (req, res) => {
+  res.send(path.join(__dirname, "../public/stats.html"));
+});  
 
 module.exports = Router;
