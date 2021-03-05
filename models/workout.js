1eb9e4aaf0c54a28d06552f1ee096627dc3ac325
James Galantino <james_galantino@aol.com>
2021-03-05 09:51:09 -0500

    Updated workout model setup

--- models/workout.js
+++ models/workout.js
@@ -7,39 +7,99 @@ const WorkoutSchema = new Schema ({
         type: Date,
         default: Date.now
     },
-    workoutType: {
-        type: String,
-        options: {
-            name: ["resistance", "cardio"],
-            required: "Please select a workout type"
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "Please select a an exercise type"
+            }
+        },
+        {
+            name: {
+                type: String,
+                required: "Please select a an exercise name"
+            }
+        },
+        {
+            duration: {
+                type: Number,
+                required: "Please enter a workout duration",
+                trim: true
+            }
+        },
+        {
+            weight: {
+                type: Number,
+                required: "Please enter a weight amount",
+                trim: true
+            }
+        },
+        {
+            reps: {
+                type: Number,
+                required: "Please enter a repetition count",
+                trim: true
+            }
+        },
+        {
+            sets: {
+                type: Number,
+                required: "Please enter a set count",
+                trim: true
+            }
+        },
+        {
+            distance: {
+                type: Number,
+                required: "Please enter a distance",
+                validate: [({ distance }) => distance > 0, "Distance must have a value greater than 0"],
+                trim: true
+            }
+        },
+        {
+            totalDuration: Number
         }
-    },
-    exercise: {
-       type: String,
-       required: "Please enter an exercise" 
-    },
-    duration: {
-        type: Number,
-        required: "Please enter a workout duration"
-    },
-    weight: {
-        type: Number,
-        required: "Please enter a weight"
-    },
-    reps: {
-        type: Number,
-        required: "Please enter a repetition count"
-    },
-    sets: {
-        type: Number,
-        required: "Please enter a set count"
-    },
-    distance: {
-        type: Number,
-        required: "Please enter a set count",
-        validate: [({ distance }) => distance > 0, "Distance must have a value greater than 0"]
-    },
+    ] 
 });
+
+// {
+//     type: String,
+//     options: {
+//         name: ["resistance", "cardio"],
+//         required: "Please select a workout type"
+//     }
+// },
+// exercise: {
+//    type: String,
+//    required: "Please enter an exercise" 
+// },
+// duration: {
+//     type: Number,
+//     required: "Please enter a workout duration",
+//     trim: true
+// },
+// weight: {
+//     type: Number,
+//     required: "Please enter a weight",
+//     trime: true
+// },
+// reps: {
+//     type: Number,
+//     required: "Please enter a repetition count",
+//     trime: true
+// },
+// sets: {
+//     type: Number,
+//     required: "Please enter a set count",
+//     trime: true
+// },
+// distance: {
+//     type: Number,
+//     required: "Please enter a set count",
+//     validate: [({ distance }) => distance > 0, "Distance must have a value greater than 0"],
+//     trim: true
+// },
+// totalDuration: Number
 
 WorkoutSchema.methods.setDate = function() {
     this.setDate = Date.now();
